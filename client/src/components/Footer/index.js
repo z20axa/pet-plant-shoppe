2f9b1a0f72c383235a684c430b484490089db260
muchirbickel <muchir99@gmail.com>
2023-02-19 23:44:24 -0500

    add js files in pages

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -25,7 +25,7 @@ const Footer = () => {
           >
             ❤️
           </span>{' '}
-          by the Tech Thoughts team.
+          by group 3 team.
         </h4>
       </div>
     </footer>
