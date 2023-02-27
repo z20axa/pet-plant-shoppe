34be58a0c46c442dceddc5a4e8984644992b29c6
muchirbickel <muchir99@gmail.com>
2023-02-27 17:57:45 -0500

    add 3pawspage

---
+++ client/src/pages/Threep/Threep.js
@@ -0,0 +1,16 @@
+import Comments from "../../components/Comments/Comment";
+
+
+const Head = () => {
+  return (
+    <div>
+      <h1>Hello monsterlessons</h1>
+      <Comments
+        commentsUrl="http://localhost:3004/comments"
+        currentUserId="1"
+      />
+    </div>
+  );
+};
+
+export default Head;
