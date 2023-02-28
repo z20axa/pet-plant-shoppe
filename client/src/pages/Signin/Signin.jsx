64aae6091bdfc8fd7cd9ab0be9cf20afd8b3f22d
z20axa <alejandrospan@hotmail.com>
2023-02-27 21:42:13 -0500

    Adds graphQL to frontend client to connect to backend

--- client/src/pages/Signin/Signin.jsx
+++ client/src/pages/Signin/Signin.jsx
@@ -1,29 +1,90 @@
 import React from "react";
-import"./Signin.scss";
+import "./Signin.scss";
 import { Link } from "react-router-dom";
 
-const Signin = ()=>{
-    return(
-<div className="login">
-<div className="card">
-<div className="left">
-    <h1>How are you?</h1>
-    <span>If you have an account, please log in.</span>
-    <button><Link style={{ textDecoration: 'none', color: 'white' }} to="/login">
-    login
-          </Link></button>
-</div>
-<div className="right">
-    <h1>Sign in</h1>
-    <form >
-<input type="text" placeholder="Username"/>
-<input type="email" placeholder="Email"/>
-<input type="password" placeholder="Password"/>
-<button>Sign in</button>
-    </form>
-</div>
-</div>
-</div>
+import { useMutation } from '@apollo/client';
+import { ADD_USER } from '../utils/mutations';
+
+import Auth from '../utils/auth';
+
+const Signin = () => {
+    // set initial form state
+    const [userFormData, setUserFormData] = useState({
+        username: '',
+        email: '',
+        password: '',
+    });
+    // set state for form validation
+    const [validated] = useState(false);
+    // set state for alert
+    const [showAlert, setShowAlert] = useState(false);
+
+    const [addUser, { error }] = useMutation(ADD_USER);
+
+    useEffect(() => {
+        if (error) {
+            setShowAlert(true);
+        } else {
+            setShowAlert(false);
+        }
+    }, [error]);
+
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setUserFormData({ ...userFormData, [name]: value });
+    };
+
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+
+        // check if form has everything (as per react-bootstrap docs)
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+
+        try {
+            const { data } = await addUser({
+                variables: { ...userFormData },
+            });
+            console.log(data);
+            Auth.login(data.addUser.token);
+        } catch (err) {
+            console.error(err);
+        }
+
+        setUserFormData({
+            username: '',
+            email: '',
+            password: '',
+        });
+    };
+
+
+
+
+    return (
+        <div className="login">
+            <div className="card">
+                <div className="left">
+                    <h1>How are you?</h1>
+                    <span>If you have an account, please log in.</span>
+                    <button><Link style={{ textDecoration: 'none', color: 'white' }} to="/login">
+                        login
+                    </Link></button>
+                </div>
+                <div className="right">
+                    <h1>Sign in</h1>
+                    <form onSubmit={handleFormSubmit}>
+                        <input type="text" onChange={handleInputChange} value={userFormData.username} placeholder="Username" />
+                        <input type="email" onChange={handleInputChange} value={userFormData.email} placeholder="Email" />
+                        <input type="password" onChange={handleInputChange} value={userFormData.password} placeholder="Password" />
+                        <button>Sign in</button>
+                    </form>
+                </div>
+            </div>
+        </div>
 
     )
 }
