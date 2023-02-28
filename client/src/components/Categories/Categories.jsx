0fddbb3890455de666a4beafec3030b7e78a2dda
natavenk <nvj971@gmail.com>
2023-02-27 21:16:32 -0500

    added images to shop

--- client/src/components/Categories/Categories.jsx
+++ client/src/components/Categories/Categories.jsx
@@ -1,10 +1,6 @@
 import React, { useState } from "react";
 import "./Categories.scss";
-import { Link, useParams } from "react-router-dom";
-import { useQuery } from "@apollo/client";
-import plantImage from "../../plant_images/africanViolet.png";
 
-import { IN_STORE } from "../../utils/queries";
 import CategoryQ from "./CategoryQ";
 
 const Categories = () => {
