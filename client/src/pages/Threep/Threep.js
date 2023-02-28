efeb037d1a871318c606f2fb264052f899802fcf
muchirbickel <muchir99@gmail.com>
2023-02-27 21:56:13 -0500

    update 3pawspage

--- client/src/pages/Threep/Threep.js
+++ client/src/pages/Threep/Threep.js
@@ -1,16 +1,16 @@
-import Comments from "../../components/Comments/Comment";
+// import Comments from "../../components/Comments/Comment";
 
 
-const Head = () => {
-  return (
-    <div>
-      <h1>Hello monsterlessons</h1>
-      <Comments
-        commentsUrl="http://localhost:3004/comments"
-        currentUserId="1"
-      />
-    </div>
-  );
-};
+// const Head = () => {
+//   return (
+//     <div>
+//       <h1>Hello monsterlessons</h1>
+//       <Comments
+//         // commentsUrl="http://localhost:3004/comments"
+//         // currentUserId="1"
+//       />
+//     </div>
+//   );
+// };
 
-export default Head;
+// export default Head;
