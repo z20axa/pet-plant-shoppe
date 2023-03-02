89074633d016e458b322b6f9c4a1e57892943e70
z20axa <alejandrospan@hotmail.com>
2023-03-01 19:50:28 -0500

    Modifies the server js file for NODE_ENV production for deployment

--- server/server.js
+++ server/server.js
@@ -18,10 +18,9 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "../client/build")));
-
-//   };
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+  };
 
 // app.get("/", (req, res) => {
 //   res.sendFile(path.join(__dirname, "../client/build/index.html"));
