e2b6fe1068be8a36cf6c26c28197d6264812c4f9
z20axa <alejandrospan@hotmail.com>
2023-03-01 19:58:03 -0500

    Modifies the server js file for NODE_ENV production for else API running

--- server/server.js
+++ server/server.js
@@ -24,6 +24,10 @@ if (process.env.NODE_ENV === "production") {
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../client/build/index.html"));
   });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API running");
+  })
 };
 
 
