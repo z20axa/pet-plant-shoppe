5e68ac8c7c19e690b3fe425250f45fa51ec2585c
muchirbickel <muchir99@gmail.com>
2023-03-01 21:27:41 -0500

    fix cart and user profile styling

--- client/src/pages/Userprofile/Userprofile.jsx
+++ client/src/pages/Userprofile/Userprofile.jsx
@@ -22,8 +22,14 @@ const Profile = () => {
     return <Navigate to="/me" />;
   }
   if (!Auth.loggedIn()) {
-    return <h1>You must be logged in</h1>;
+    return <div className="logpic"><h9>You should be logged in to see more!</h9>          <img
+    src="/img/typing-cat.gif"
+    alt=""
+  />
+    </div>;
   }
+
+  
   if (loading) {
     return <div>Loading...</div>;
   }
@@ -36,9 +42,10 @@ const Profile = () => {
     <div className="box">
 
       <h2 className="card-header">Hello {profile.username}!</h2>
-      <p> List of your Favorite Plants </p>
+      <p> Check the List of your Favorite Plants </p>
+      <h5>
       {profile.plant && profile.plant.map((plant) => plant.name)}
-
+      </h5>
     </div>
   );
 };
