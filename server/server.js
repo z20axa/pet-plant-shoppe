5e9126568fbe3a42ff6dad11151732021da27c26
z20axa <alejandrospan@hotmail.com>
2023-03-01 19:56:06 -0500

    Modifies the server js file for NODE_ENV production for build/index.html

--- server/server.js
+++ server/server.js
@@ -20,11 +20,13 @@ app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
-  };
 
-// app.get("/", (req, res) => {
-//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
-// });
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  });
+};
+
+
 
 const server = new ApolloServer({
   typeDefs,
